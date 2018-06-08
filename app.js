5082b5aeb3783909480565e6dbb058c60552e681
FuentesFelipe <38222911+FuentesFelipe@users.noreply.github.com>
2018-06-08 01:13:08 -0400

    weando con firebase

---
+++ app.js
@@ -0,0 +1,8 @@
+//login
+var provider = new firebase.auth.GoogleAuthProvider();
+
+$('#login').click(function(){
+    firebase.auth().signInWithPopup(provider).then(function(result ) {
+        console.log(result.user);
+    });
+});
