89dc39fe4a24ff1fd593d0ae135c3538c3022fb0
FuentesFelipe <38222911+FuentesFelipe@users.noreply.github.com>
2018-06-08 17:41:04 -0400

    borrar

--- app.js
+++
@@ -1,8 +0,0 @@
-//login
-var provider = new firebase.auth.GoogleAuthProvider();
-
-$('#login').click(function(){
-    firebase.auth().signInWithPopup(provider).then(function(result ) {
-        console.log(result.user);
-    });
-});
